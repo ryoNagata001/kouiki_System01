7eff260645d0531011b79025018be5a701d02aea
ryoNagata001 <ryo.nagata.utokyo@gmail.com>
2017-01-23 14:02:30 +0900

    Update ofApp.cpp

--- ofxBox2dTest/src/ofApp.cpp
+++ ofxBox2dTest/src/ofApp.cpp
@@ -7,45 +7,46 @@ void ofApp::setup(){
     ofSetVerticalSync(true);
     ofBackground(0, 0, 0);
     
-    //box2Dの環境設定
+    //ボールがゴールに入っていない状態にする
+    circle_in = false;
+    
+    //box2Dの物理世界のパラメーターを設定
     box2d.init();
     box2d.setGravity(0,5);
     box2d.createBounds(0, 0, ofGetWidth(), ofGetHeight());
     box2d.setFPS(30);
     
-//    for (int i = 0; i < 100; i++){
-//        ofPtr<ofxBox2dRect> r = ofPtr<ofxBox2dRect>(new ofxBox2dRect);
-//        float w = 2;
-//        float h = 2;
-//        float x = ofRandom(50, ofGetWidth() - 50);
-//        float y = ofRandom(50, ofGetHeight() - 50);
-//        //r.get()->setPhysics(1000, 1, 0);
-//        r.get()->setup(box2d.getWorld(), x, y, w, h);
-//        
-//        rects.push_back(r);
-//    }
-    //バスケットゴール用の輪郭線の設定
+    //ゴールを作る
     ofPtr<ofxBox2dRect> r1 = ofPtr<ofxBox2dRect>(new ofxBox2dRect);
-    r1.get()->setup(box2d.getWorld(), ofGetWidth()/2-50,ofGetHeight()-25, 2, 50);
+    r1.get()->setup(box2d.getWorld(), ofGetWidth()/2-50,ofGetHeight()-25, 2, 70);
     rects.push_back(r1);
     ofPtr<ofxBox2dRect> r2 = ofPtr<ofxBox2dRect>(new ofxBox2dRect);
-    r2.get()->setup(box2d.getWorld(), ofGetWidth()/2+50,ofGetHeight()-25, 2, 50);
+    r2.get()->setup(box2d.getWorld(), ofGetWidth()/2+50,ofGetHeight()-25, 2, 70);
     rects.push_back(r2);
     ofPtr<ofxBox2dRect> r3 = ofPtr<ofxBox2dRect>(new ofxBox2dRect);
-    r3.get()->setup(box2d.getWorld(), ofGetWidth()/2,ofGetHeight()-100, 100, 2);
+    r3.get()->setup(box2d.getWorld(), ofGetWidth()/2,ofGetHeight()-150, 300, 2);
     rects.push_back(r3);
 }
 
 //--------------------------------------------------------------
 void ofApp::update(){
-    //アニメーションのアップデート
+    //物理演算
     box2d.update();
+    //ボールが入ったか否かを判断(ネストが深すぎる)
+    for(int i = 0; i < circles.size(); i++){
+        if (circles[i]->getPosition().x <= ofGetWidth()/2+50){
+            if(circles[i]->getPosition().x >= ofGetWidth()/2-50){
+                if(circles[i]->getPosition().y >= ofGetHeight()-50){
+                    circle_in = true;
+                }
+            }
+        }
+    }
 }
 
 //--------------------------------------------------------------
 void ofApp::draw(){
-    //アニメーションの描画
-    //円の描画
+    //円を一つづつ描画
     for (int i = 0; i < circles.size(); i++){
         ofFill();
         ofSetColor(0, 127, 255);
@@ -53,34 +54,38 @@ void ofApp::draw(){
         // circles[i].draw();
     }
     
-    //バスケットゴールの描画
+    //ゴールを描画
     for(int i = 0; i < 3; i++){
         ofFill();
         ofSetColor(255, 255, 255);
         rects[i].get()->draw();
         // circles[i].draw();
     }
-    
-    //ゴールに色をつけた
+    //ゴールに色付け
     ofSetColor(255, 0, 0, 125);
     ofDrawRectangle(ofGetWidth()/2-50, ofGetHeight()-25, 100, 50);
     
+    //ルールの記述
+    ofDrawBitmapString( "reset: r, ball: click", 30, 30);
     
+    //クリア判定
+    if(circle_in){
+        string thresholdMsg = "CLEAR!!!";
+        ofDrawBitmapString( thresholdMsg, ofGetWidth()/2, ofGetHeight()/2);
+    }
+
     box2d.draw();
-//    for (int i = 0; i < circles.size(); i++){
-//        ofFill();
-//        ofSetColor(0, 127, 255);
-//        circles[i].draw();
-//    }
 }
 
-//rを押すと球が全て消えるようにした--------------------------------------------------------------
+//--------------------------------------------------------------
 void ofApp::keyPressed(int key){
+    //ゲームのリセット
     if (key == 'r'){
         for(int i = 0; i < circles.size(); i++){
             circles[i].get()->destroy();
         }
         circles.clear();
+        circle_in = false;
     }
 }
 
@@ -99,9 +104,11 @@ void ofApp::mouseDragged(int x, int y, int button){
 
 }
 
-//mouseをクリックした時、y座標の値がゴールより上だったら、ボールを落とせるようにした--------------------------------------------------------------
+//--------------------------------------------------------------
 void ofApp::mousePressed(int x, int y, int button){
-    if(y <= ofGetHeight()-100){
+    
+    //ゴールより上の部分でボールを落とせる
+    if(y <= ofGetHeight()-150){
     float r = ofRandom(10, 40);
     ofPtr<ofxBox2dCircle> circle = ofPtr<ofxBox2dCircle>(new ofxBox2dCircle);
     circle.get()->setPhysics(1.0, 0.8, 0.5);
