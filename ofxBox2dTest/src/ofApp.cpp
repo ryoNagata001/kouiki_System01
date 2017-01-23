bf5300d0ed306d7ccb70a30dd68ccd6bd09a45d9
ryoNagata001 <ryo.nagata.utokyo@gmail.com>
2017-01-23 09:58:49 +0900

    Update ofApp.cpp

--- ofxBox2dTest/src/ofApp.cpp
+++ ofxBox2dTest/src/ofApp.cpp
@@ -2,10 +2,12 @@
 
 //--------------------------------------------------------------
 void ofApp::setup(){
+    //描画の基本設定
     ofSetFrameRate(60);
     ofSetVerticalSync(true);
     ofBackground(0, 0, 0);
     
+    //box2Dの環境設定
     box2d.init();
     box2d.setGravity(0,5);
     box2d.createBounds(0, 0, ofGetWidth(), ofGetHeight());
@@ -22,7 +24,7 @@ void ofApp::setup(){
 //        
 //        rects.push_back(r);
 //    }
-    
+    //バスケットゴール用の輪郭線の設定
     ofPtr<ofxBox2dRect> r1 = ofPtr<ofxBox2dRect>(new ofxBox2dRect);
     r1.get()->setup(box2d.getWorld(), ofGetWidth()/2-50,ofGetHeight()-25, 2, 50);
     rects.push_back(r1);
@@ -36,11 +38,14 @@ void ofApp::setup(){
 
 //--------------------------------------------------------------
 void ofApp::update(){
+    //アニメーションのアップデート
     box2d.update();
 }
 
 //--------------------------------------------------------------
 void ofApp::draw(){
+    //アニメーションの描画
+    //円の描画
     for (int i = 0; i < circles.size(); i++){
         ofFill();
         ofSetColor(0, 127, 255);
@@ -48,15 +53,18 @@ void ofApp::draw(){
         // circles[i].draw();
     }
     
+    //バスケットゴールの描画
     for(int i = 0; i < 3; i++){
         ofFill();
         ofSetColor(255, 255, 255);
         rects[i].get()->draw();
         // circles[i].draw();
     }
+    
+    //ゴールに色をつけた
     ofSetColor(255, 0, 0, 125);
     ofDrawRectangle(ofGetWidth()/2-50, ofGetHeight()-25, 100, 50);
-
+    
     
     box2d.draw();
 //    for (int i = 0; i < circles.size(); i++){
@@ -66,7 +74,7 @@ void ofApp::draw(){
 //    }
 }
 
-//--------------------------------------------------------------
+//rを押すと球が全て消えるようにした--------------------------------------------------------------
 void ofApp::keyPressed(int key){
     if (key == 'r'){
         for(int i = 0; i < circles.size(); i++){
@@ -91,7 +99,7 @@ void ofApp::mouseDragged(int x, int y, int button){
 
 }
 
-//--------------------------------------------------------------
+//mouseをクリックした時、y座標の値がゴールより上だったら、ボールを落とせるようにした--------------------------------------------------------------
 void ofApp::mousePressed(int x, int y, int button){
     if(y <= ofGetHeight()-100){
     float r = ofRandom(10, 40);
